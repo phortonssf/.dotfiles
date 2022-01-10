b13196cce45625f844ff480e2bce3b86cc9fb858
Peter Horton <phorton@softstackfactory.com>
2022-01-09 17:31:25 -0800

    (clean) completion and removes unused

--- zsh/.zsh-plugins.zsh
+++ zsh/.zsh-plugins.zsh
@@ -37,9 +37,6 @@ plugins=(
 
 zstyle ':completion::*:ls::*' fzf-completion-opts --preview='eval head {1}'
 source $HOME/.oh-my-zsh/custom/plugins/fzf-tab-completion/zsh/fzf-zsh-completion.sh
-# zstyle ':completion:*:*:aws' fzf-search-display true
- # bindkey '^I' fzf_completion
- # zstyle ':completion:*:*:git:*' fzf-search-display true
 
 ## use ripgrep with FZF
 if type rg &> /dev/null; then
@@ -53,12 +50,23 @@ export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
+zstyle ':completion:*' auto-description 'specify: %d'
+zstyle ':completion:*' completer _expand _complete _correct _approximate
+zstyle ':completion:*' format 'Completing %d'
+zstyle ':completion:*' group-name ''
+zstyle ':completion:*' menu select=2
+zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*' list-colors ''
+zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
+zstyle ':completion:*' matcher-list '' 'm:{a-z}={A-Z}' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=* l:|=*'
+zstyle ':completion:*' menu select=long
+zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
+zstyle ':completion:*' use-compctl false
+zstyle ':completion:*' verbose true
+zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;31'
+zstyle ':completion:*:kill:*' command 'ps -u $USER -o pid,%cpu,tty,cputime,cmd'
+
 complete -o nospace -C ~/bin/terraform terraform
 source <(kubectl completion zsh)
 complete -C aws_completer aws
-# bindkey -s jj '\e'
-# bindkey jj vi-cmd-mode
-# bindkey '^ ' autosuggest-accept
-
-# zstyle ':completion:*' menu select
 
