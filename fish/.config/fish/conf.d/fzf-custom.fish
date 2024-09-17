745c947374ef67b6c4fe5b83c59fb2feba44531d
Peter Horton <phorton@softstackfactory.com>
2024-09-16 19:43:58 -0700

    feat(fzf): fixes FZF_DEFAULT_OPTS adds catppuccin

--- fish/.config/fish/conf.d/fzf-custom.fish
+++ fish/.config/fish/conf.d/fzf-custom.fish
@@ -8,10 +8,18 @@
 # set -Ux FZF_DEFAULT_OPTS "\
 # --color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
 # --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
-# --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
-# --color=selected-bg:#45475a \
-# --multi"
+#
+# # --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
+# # --color=selected-bg:#45475a \
+# # --multi"
 # set -Ux FZF_TMUX_OPTS "-p 55%,60%"
 #
 # set -Ux FZF_CTRL_R_OPTS "--border-label=' history ' \
 # --prompt='  '"
+set -Ux FZF_DEFAULT_OPTS "
+--border=rounded --height 50% --min-height=30 --layout=reverse \
+--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8 \
+--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
+--color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
+--color=selected-bg:#45475a \
+--multi"
